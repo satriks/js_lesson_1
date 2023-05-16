d7e7f226c5aa8eaf62c43e9849bee5b957d1c02c
Yury Vereskun <satriks@mail.ru>
2023-05-16 23:54:59 +0300

    initial commit

---
+++ gessNumber.js
@@ -0,0 +1,27 @@
+
+
+const randomNumber = Math.floor(Math.random() * 1000);
+
+console.log("Загаданно :", randomNumber)
+
+while (true ) {
+
+    let tryNumber = prompt("Введите число от 0 до 1000. (для выхода q)")
+    
+    tryNumber = +tryNumber
+
+    if (isNaN(tryNumber)) {
+        alert("Это не число !")
+    } else if (tryNumber == randomNumber) {
+        alert ("Вы угадали");
+        break
+    } else if (tryNumber > randomNumber) {
+        alert ("Загаданное число  меньше")
+    } else if (tryNumber == "q"){
+        break
+    }  else {
+        alert("Загаданное число больше")
+    }
+
+    
+}
